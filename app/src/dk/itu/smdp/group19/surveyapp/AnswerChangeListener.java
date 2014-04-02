882d59419b24c8c2663e1ea56c1cd41c0f512afb
Per Mortensen <per@permortensen.com>
2014-04-02 21:29:12 +0200

    Added user control generator to android app project.

---
+++ app/src/dk/itu/smdp/group19/surveyapp/AnswerChangeListener.java
@@ -0,0 +1,18 @@
+package dk.itu.smdp.group19.surveyapp;
+
+import android.view.View;
+
+/**
+ * Interface for receiving an event when the user changes an answer to a question,
+ * e.g. when selecting a check box.
+ * For multi choice answers, {onAnswerChanged} will be called both when a check box
+ * is checked and unchecked.
+ * For single choice answers, it will be called only when a radio button is selected.
+ * For free text answers, it will be called when the text in the text box is changed.
+ * For free text answers inside choice answers, it will be called in any of these
+ * scenarios.
+ * @author per
+ */
+public interface AnswerChangeListener {
+	public void onAnswerChanged(String answerText, View answerView);
+}
