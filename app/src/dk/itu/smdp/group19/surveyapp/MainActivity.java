2f9803d1be0afec8b50997a8a6442e261ad5c1ef
Mathias Brandt <maini123@gmail.com>
2014-04-02 13:47:20 +0200

    Created app project.

---
+++ app/src/dk/itu/smdp/group19/surveyapp/MainActivity.java
@@ -0,0 +1,13 @@
+package dk.itu.smdp.group19.surveyapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
