c5ca07955b7bc36910ab9519daac210d5803088d
Mathias Brandt <maini123@gmail.com>
2014-04-22 15:09:45 +0200

    added some additional methods

--- app/src/dk/itu/smdp/group19/surveyapp/parser/UserControlGenerator.java
+++ app/src/dk/itu/smdp/group19/surveyapp/parser/UserControlGenerator.java
@@ -6,10 +6,19 @@ import java.util.Map.Entry;
 import android.content.Context;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
-import android.widget.*;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import dk.itu.smdp.group19.surveyapp.parser.elements.Page;
+import dk.itu.smdp.group19.surveyapp.parser.elements.Question;
 
 /**
  * Factory class for creating Android user controls (Views) for answers and answer groups.
@@ -28,7 +37,7 @@ public class UserControlGenerator {
 	 * @param acl The event handler for when the answer is changed.
 	 * @return A TextEdit instance.
 	 */
-	public EditText makeFreetextAnswer(String hint, final AnswerChangeListener acl) {
+	private EditText makeFreetextAnswer(String hint, final AnswerChangeListener acl) {
 		final EditText et = new EditText(context);
 		et.setHint(hint);
 		et.addTextChangedListener(new TextWatcher() {
@@ -46,6 +55,21 @@ public class UserControlGenerator {
 		return et;
 	}
 	
+	public ViewGroup makeFreetextAnswers(Map<String, Boolean> answers, final AnswerChangeListener acl) {
+		LinearLayout ll = new LinearLayout(context);
+		ll.setOrientation(LinearLayout.VERTICAL);
+		LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+		ll.setLayoutParams(lparams);
+//		lparams.addRule(RelativeLayout.CENTER_VERTICAL); // center text box, i hope
+		
+		for(Entry<String, Boolean> answer : answers.entrySet()) {
+			EditText et = makeFreetextAnswer(answer.getKey(), acl);
+			ll.addView(et);
+		}
+		
+		return ll;
+	}
+	
 	/**
 	 * Creates a multiple choice answer group where each answer may be a pre-defined answer or a free-text answer.
 	 * @param answers A map of answers, where the key is the answer text and the value is a boolean denoting if it is free-text (true) or not (false).
@@ -53,9 +77,11 @@ public class UserControlGenerator {
 	 * @return A ViewGroup of the answers containing checkboxes.
 	 */
 	public ViewGroup makeMultiChoiceAnswers(Map<String, Boolean> answers, final AnswerChangeListener acl) {
-		RelativeLayout ll = new RelativeLayout(context);
-		RelativeLayout.LayoutParams lparams = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-		lparams.addRule(RelativeLayout.CENTER_VERTICAL); // center text box, i hope
+		LinearLayout ll = new LinearLayout(context);
+		ll.setOrientation(LinearLayout.VERTICAL);
+		LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+		ll.setLayoutParams(lparams);
+//		lparams.addRule(RelativeLayout.CENTER_VERTICAL); // center text box, i hope
 		
 		for(Entry<String, Boolean> answer : answers.entrySet()) {
 			final CheckBox cb = makeCheckBox(answer.getKey());
@@ -129,6 +155,36 @@ public class UserControlGenerator {
 		}
 		
 		return rg;
+	}
+	
+	public View makeRuler(int color) {
+		View ruler = new View(context);
+		ruler.setBackgroundColor(color);
+		ruler.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 2));
+		
+		return ruler;
+	}
+	
+	public ViewGroup makePageHeader(Page page) {
+		LinearLayout layout = new LinearLayout(context);
+		layout.setOrientation(LinearLayout.VERTICAL);
+		
+		TextView pageTitle = new TextView(context);
+		pageTitle.setText(page.getName());
+		layout.addView(pageTitle);
+		
+		TextView pageDescription = new TextView(context);
+		pageDescription.setText(page.getDescription());
+		layout.addView(pageDescription);
+		
+		return layout;
+	}
+	
+	public View makeQuestionHeader(Question question) {
+		TextView questionName = new TextView(context);
+		questionName.setText(question.getName());
+		
+		return questionName;
 	}
 	
 	private CheckBox makeCheckBox(String text) {
