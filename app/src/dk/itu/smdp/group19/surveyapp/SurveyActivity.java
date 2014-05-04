76c44badc4d4b11af7fe390c78a13e6b0c03562f
Simon Langhoff <simlanghoff@gmail.com>
2014-05-04 14:16:26 +0200

    Fixed radio button controls and layout. Now proper layout and enable mechanism.

--- app/src/dk/itu/smdp/group19/surveyapp/SurveyActivity.java
+++ app/src/dk/itu/smdp/group19/surveyapp/SurveyActivity.java
@@ -28,34 +28,35 @@ public class SurveyActivity extends Activity {
 	public final String TAG = "SurveyActivity";
 	private final String APPDIR = MainActivity.getAppDir();
 	private String SURVEY_FILE_NAME;
-	
+
 	private UserControlGenerator controlGenerator;
 	private List<Question> allQuestions;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_survey);
-		
+
 		SURVEY_FILE_NAME = getIntent().getStringExtra("filename");
-		
+
 		String filePath = APPDIR + "/" + SURVEY_FILE_NAME;
-		
+
 		controlGenerator = new UserControlGenerator(this);
-		
+
 		XmlParser parser = new XmlParser(filePath);
 		Survey survey = parser.parse();
-		
-		if(survey != null)
+
+		if (survey != null)
 			generateSurvey(survey);
-		
+
 		allQuestions = collectQuestions(survey);
 		applyDependencies(allQuestions);
-		
-		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+		getWindow().setSoftInputMode(
+				WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 		removeFocusFromAllElements();
 	}
-	
+
 	private void generateSurvey(Survey survey) {
 		// make it possible to enable/disable questions based on dependencies
 		Action answerChangedCallback = new Action() {
@@ -64,86 +65,104 @@ public class SurveyActivity extends Activity {
 				applyDependencies(allQuestions);
 			}
 		};
-		
+
 		LinearLayout layout = (LinearLayout) findViewById(R.id.root);
-		
+
 		TextView surveyTitle = new TextView(this);
 		surveyTitle.setText(survey.getTitle());
 		layout.addView(surveyTitle);
-		
-		for(Page page : survey.getPages()) {
+
+		for (Page page : survey.getPages()) {
 			layout.addView(controlGenerator.makePageHeader(page));
-			
-			if(page.getClass() == QuestionPage.class) {
+
+			if (page.getClass() == QuestionPage.class) {
 				QuestionPage questionPage = (QuestionPage) page;
-				
-				for(Question question : questionPage.getQuestions()) {
-					layout.addView(controlGenerator.makeQuestionHeader(question));
-					
+
+				for (Question question : questionPage.getQuestions()) {
+					layout.addView(controlGenerator
+							.makeQuestionHeader(question));
+
 					ViewGroup answers = null;
-					
-					if(question.getType() == QuestionType.SINGLE) {
-						answers = controlGenerator.makeSingleChoiceAnswers(question.getAnswers(), new DefaultAnswerChangeListener(answerChangedCallback));
+
+					if (question.getType() == QuestionType.SINGLE) {
+						answers = controlGenerator.makeSingleChoiceAnswers(
+								question.getAnswers(),
+								new DefaultAnswerChangeListener(
+										answerChangedCallback));
+					} else if (question.getType() == QuestionType.MULTI) {
+						answers = controlGenerator.makeMultiChoiceAnswers(
+								question.getAnswers(),
+								new DefaultAnswerChangeListener(
+										answerChangedCallback));
+					} else if (question.getType() == QuestionType.FREETEXT) {
+						answers = controlGenerator.makeFreetextAnswers(question
+								.getAnswers(), new DefaultAnswerChangeListener(
+								answerChangedCallback));
 					}
-					else if(question.getType() == QuestionType.MULTI) {
-						answers = controlGenerator.makeMultiChoiceAnswers(question.getAnswers(), new DefaultAnswerChangeListener(answerChangedCallback));
-					}
-					else if(question.getType() == QuestionType.FREETEXT) {
-						answers = controlGenerator.makeFreetextAnswers(question.getAnswers(), new DefaultAnswerChangeListener(answerChangedCallback));
-					}
-					
+
 					layout.addView(answers);
 					question.setAnswerViewGroup(answers);
 				}
 			}
 		}
-		
+
 		Button buttonSend = new Button(this);
 		buttonSend.setText("Send");
 		buttonSend.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				removeFocusFromAllElements();
-				
+
 				Intent i = new Intent(v.getContext(), SendEmailActivity.class);
 				startActivity(i);
 			}
 		});
 		layout.addView(buttonSend);
 	}
-	
+
 	private void applyDependencies(List<Question> allQuestions) {
-		for(Question q : allQuestions) {
-			if(q.getRequires() == null || q.getAnswerViewGroup() == null)
+		for (Question q : allQuestions) {
+			if (q.getRequires() == null || q.getAnswerViewGroup() == null)
 				continue;
-			
+
 			// enable or disable the question
 			ViewGroup vg = q.getAnswerViewGroup();
-			boolean enable = AnswerCollector.isDependencySatisfied(q.getRequires());
-			
-			for(int i = 0; i < vg.getChildCount(); i++)
-				vg.getChildAt(i).setEnabled(enable);
-			vg.setEnabled(enable);
+
+			boolean enable = AnswerCollector.isDependencySatisfied(q
+					.getRequires());
+
+			setGroupEnabled(vg, enable);
 		}
 	}
-	
+
 	private List<Question> collectQuestions(Survey s) {
 		ArrayList<Question> questions = new ArrayList<Question>();
-		
-		for(Page p : s.getPages()) {
-			if(p instanceof QuestionPage)
+
+		for (Page p : s.getPages()) {
+			if (p instanceof QuestionPage)
 				questions.addAll(((QuestionPage) p).getQuestions());
 		}
-		
+
 		return questions;
 	}
-	
+
 	private void removeFocusFromAllElements() {
 		LinearLayout ll = (LinearLayout) findViewById(R.id.root);
-		
-		for(int i = 0; i < ll.getChildCount(); i++) {
+
+		for (int i = 0; i < ll.getChildCount(); i++) {
 			View v = ll.getChildAt(i);
 			v.clearFocus();
 		}
+	}
+
+	private void setGroupEnabled(ViewGroup view, boolean enabled) {
+		for (int i = 0; i < view.getChildCount(); i++) {
+			View v = view.getChildAt(i);
+			if (v instanceof ViewGroup)
+				setGroupEnabled((ViewGroup) v, enabled);
+			else
+				v.setEnabled(enabled);
+		}
+		view.setEnabled(enabled);
 	}
 }
--- app/src/dk/itu/smdp/group19/surveyapp/parser/UserControlGenerator.java
+++ app/src/dk/itu/smdp/group19/surveyapp/parser/UserControlGenerator.java
@@ -20,176 +20,210 @@ import dk.itu.smdp.group19.surveyapp.parser.elements.Page;
 import dk.itu.smdp.group19.surveyapp.parser.elements.Question;
 
 /**
- * Factory class for creating Android user controls (Views) for answers and answer groups.
+ * Factory class for creating Android user controls (Views) for answers and
+ * answer groups.
+ * 
  * @author per
  */
 public class UserControlGenerator {
-	private Context context;
-	
+	private final Context context;
+
 	public UserControlGenerator(Context context) {
 		this.context = context;
 	}
-	
+
 	/**
 	 * Creates a TextEdit view representing a free-text answer.
-	 * @param hint The text of the answer.
-	 * @param acl The event handler for when the answer is changed.
+	 * 
+	 * @param hint
+	 *            The text of the answer.
+	 * @param acl
+	 *            The event handler for when the answer is changed.
 	 * @return A TextEdit instance.
 	 */
-	private EditText makeFreetextAnswer(final Answer answer, final AnswerChangeListener acl) {
+	private EditText makeFreetextAnswer(final Answer answer,
+			final AnswerChangeListener acl) {
 		final EditText et = new EditText(context);
 		et.setHint(answer.getName());
 		et.setOnFocusChangeListener(new OnFocusChangeListener() {
-			
+
 			@Override
 			public void onFocusChange(View v, boolean hasFocus) {
-				if(!hasFocus) {
+				if (!hasFocus) {
 					// check if text field actually has content
-					if(((EditText) v).getText().toString().trim().length() > 0) {
+					if (((EditText) v).getText().toString().trim().length() > 0) {
 						acl.onAnswerChanged(answer, et);
 					}
 				}
 			}
 		});
-		
-//		et.addTextChangedListener(new TextWatcher() {
-//			@Override
-//			public void onTextChanged(CharSequence s, int start, int before, int count) { /* do nothing */ }
-//			@Override
-//			public void beforeTextChanged(CharSequence s, int start, int count, int after) { /* do nothing */ }
-//			
-//			@Override
-//			public void afterTextChanged(Editable s) {
-//				acl.onAnswerChanged(answer, et);
-//			}
-//		});
-		
+
+		// et.addTextChangedListener(new TextWatcher() {
+		// @Override
+		// public void onTextChanged(CharSequence s, int start, int before, int
+		// count) { /* do nothing */ }
+		// @Override
+		// public void beforeTextChanged(CharSequence s, int start, int count,
+		// int after) { /* do nothing */ }
+		//
+		// @Override
+		// public void afterTextChanged(Editable s) {
+		// acl.onAnswerChanged(answer, et);
+		// }
+		// });
+
 		return et;
 	}
-	
-	public ViewGroup makeFreetextAnswers(ArrayList<Answer> answers, final AnswerChangeListener acl) {
+
+	public ViewGroup makeFreetextAnswers(ArrayList<Answer> answers,
+			final AnswerChangeListener acl) {
 		LinearLayout ll = new LinearLayout(context);
 		ll.setOrientation(LinearLayout.VERTICAL);
-		LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+		LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(
+				LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
 		ll.setLayoutParams(lparams);
-//		lparams.addRule(RelativeLayout.CENTER_VERTICAL); // center text box, i hope
-		
-		for(Answer answer : answers) {
+		// lparams.addRule(RelativeLayout.CENTER_VERTICAL); // center text box,
+		// i hope
+
+		for (Answer answer : answers) {
 			EditText et = makeFreetextAnswer(answer, acl);
 			ll.addView(et);
 		}
-		
+
 		return ll;
 	}
-	
+
 	/**
-	 * Creates a multiple choice answer group where each answer may be a pre-defined answer or a free-text answer.
-	 * @param answers A map of answers, where the key is the answer text and the value is a boolean denoting if it is free-text (true) or not (false).
-	 * @param acl The event handler.
+	 * Creates a multiple choice answer group where each answer may be a
+	 * pre-defined answer or a free-text answer.
+	 * 
+	 * @param answers
+	 *            A map of answers, where the key is the answer text and the
+	 *            value is a boolean denoting if it is free-text (true) or not
+	 *            (false).
+	 * @param acl
+	 *            The event handler.
 	 * @return A ViewGroup of the answers containing checkboxes.
 	 */
-	public ViewGroup makeMultiChoiceAnswers(ArrayList<Answer> answers, final AnswerChangeListener acl) {
+	public ViewGroup makeMultiChoiceAnswers(ArrayList<Answer> answers,
+			final AnswerChangeListener acl) {
 		LinearLayout ll = new LinearLayout(context);
 		ll.setOrientation(LinearLayout.VERTICAL);
-		LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+		LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(
+				LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
 		ll.setLayoutParams(lparams);
-//		lparams.addRule(RelativeLayout.CENTER_VERTICAL); // center text box, i hope
-		
-		for(final Answer answer : answers) {
-			if(answer.isFreetext()) {
+		// lparams.addRule(RelativeLayout.CENTER_VERTICAL); // center text box,
+		// i hope
+
+		for (final Answer answer : answers) {
+			if (answer.isFreetext()) {
 				// add textbox
 				TextView tv = new TextView(context);
 				tv.setText("Other: ");
 				ll.addView(tv);
-				
+
 				EditText et = makeFreetextAnswer(answer, acl);
 				ll.addView(et);
-			}
-			else {
+			} else {
 				final CheckBox cb = makeCheckBox(answer.getName());
 				ll.addView(cb);
-				
+
 				cb.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 					@Override
-					public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+					public void onCheckedChanged(CompoundButton buttonView,
+							boolean isChecked) {
 						acl.onAnswerChanged(answer, cb);
 					}
 				});
 			}
 		}
-		
+
 		return ll;
 	}
-	
-	
+
 	/**
-	 * Creates a single choice answer group where each answer may be a pre-defined answer or a free-text answer.
-	 * @param answers A map of answers, where the key is the answer text and the value is a boolean denoting if it is free-text (true) or not (false).
-	 * @param acl The event handler.
+	 * Creates a single choice answer group where each answer may be a
+	 * pre-defined answer or a free-text answer.
+	 * 
+	 * @param answers
+	 *            A map of answers, where the key is the answer text and the
+	 *            value is a boolean denoting if it is free-text (true) or not
+	 *            (false).
+	 * @param acl
+	 *            The event handler.
 	 * @return A ViewGroup of the answers containing radio buttons.
 	 */
-	public ViewGroup makeSingleChoiceAnswers(ArrayList<Answer> answers, final AnswerChangeListener acl) {
+	public ViewGroup makeSingleChoiceAnswers(ArrayList<Answer> answers,
+			final AnswerChangeListener acl) {
 		LinearLayout ll = new LinearLayout(context);
+		ll.setOrientation(LinearLayout.VERTICAL);
+		LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(
+				LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+		ll.setLayoutParams(lparams);
+
 		RadioGroup rg = new RadioGroup(context);
-		
-		for(final Answer answer : answers) {
+
+		for (final Answer answer : answers) {
 			final RadioButton rb = makeRadioButton(answer.getName());
 			rg.addView(rb);
-			
-			if(answer.isFreetext()) {
+
+			if (answer.isFreetext()) {
 				// add textbox
 				EditText et = makeFreetextAnswer(answer, acl);
-				ll.addView(et);
+				rg.addView(et);
 			}
-			
+
 			rb.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 				@Override
-				public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-					if(isChecked)
+				public void onCheckedChanged(CompoundButton buttonView,
+						boolean isChecked) {
+					if (isChecked)
 						acl.onAnswerChanged(answer, rb);
 				}
 			});
 		}
-		
-		return rg;
+
+		ll.addView(rg);
+		return ll;
 	}
-	
+
 	public View makeRuler(int color) {
 		View ruler = new View(context);
 		ruler.setBackgroundColor(color);
-		ruler.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 2));
-		
+		ruler.setLayoutParams(new ViewGroup.LayoutParams(
+				ViewGroup.LayoutParams.MATCH_PARENT, 2));
+
 		return ruler;
 	}
-	
+
 	public ViewGroup makePageHeader(Page page) {
 		LinearLayout layout = new LinearLayout(context);
 		layout.setOrientation(LinearLayout.VERTICAL);
-		
+
 		TextView pageTitle = new TextView(context);
 		pageTitle.setText(page.getName());
 		layout.addView(pageTitle);
-		
+
 		TextView pageDescription = new TextView(context);
 		pageDescription.setText(page.getDescription());
 		layout.addView(pageDescription);
-		
+
 		return layout;
 	}
-	
+
 	public View makeQuestionHeader(Question question) {
 		TextView questionName = new TextView(context);
 		questionName.setText(question.getName());
-		
+
 		return questionName;
 	}
-	
+
 	private CheckBox makeCheckBox(String text) {
 		CheckBox cb = new CheckBox(context);
 		cb.setText(text);
 		return cb;
 	}
-	
+
 	private RadioButton makeRadioButton(String text) {
 		RadioButton rb = new RadioButton(context);
 		rb.setText(text);
